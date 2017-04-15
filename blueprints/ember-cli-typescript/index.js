4b37f8ede545084fd400430000403f72c715b5d3
Chris Krycho <chris@chriskrycho.com>
2017-04-15 09:05:30 -0400

    Try using `path.sep` to fix Windows path issues.

--- blueprints/ember-cli-typescript/index.js
+++ blueprints/ember-cli-typescript/index.js
@@ -7,16 +7,20 @@ module.exports = {
     return [
       path.join(this.path, 'files', 'tsconfig.json'),
       path.join(this.path, 'files', 'app', 'config', 'environment.d.ts')
-      ];
+    ];
   },
 
-   mapFile: function() {
-    var result = this._super.mapFile.apply(this, arguments);
-    if (result.indexOf('/tsconfig.json')>-1) {
+  mapFile: function() {
+    const result = this._super.mapFile.apply(this, arguments);
+
+    const tsconfigPattern = `${path.sep}tsconfig.json`;
+    const appPattern = `${path.sep}app${path.sep}`;
+
+    if (result.indexOf(tsconfigPattern) > -1) {
       return 'tsconfig.json';
-    } else if (result.indexOf('/app/')>-1) {
+    } else if (result.indexOf('/app/') > -1) {
       var pos = result.indexOf('/app/');
-      return result.substring(pos+1);
+      return result.substring(pos + 1);
     }
   },
 
