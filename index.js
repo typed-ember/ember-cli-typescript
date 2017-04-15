affc1003f0a4ba5b36e50bfe3178deb50a30934e
Chris Krycho <chris@chriskrycho.com>
2017-04-15 16:44:55 -0400

    Lazily load typescript preprocessor, informing user of lazy load.
    Since the typescript dependencies *are* available (by way of the blueprint)
    after installation, add a note indicating this absence is safe during the
    install process but is a problem during build, serve, and test.

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 'use strict';
 var path      = require('path');
 var process = require('process');
-var TsPreprocessor = require('./lib/typescript-preprocessor');
+
+let TsPreprocessor;
+try {
+  TsPreprocessor = require('./lib/typescript-preprocessor');
+} catch ( ex ) {
+  // Do nothing; we just won't have the plugin available. This means that if you
+  // somehow end up in a state where it doesn't load, the preprocessor *will*
+  // fail, but this is necessary because the preprocessor depends on packages
+  // which aren't installed until the
+}
 
 module.exports = {
   name: 'ember-cli-typescript',
@@ -20,6 +29,11 @@ module.exports = {
   },
 
   setupPreprocessorRegistry: function(type, registry) {
+    if (!TsPreprocessor) {
+      console.log("Note: TypeScript preprocessor not available -- some dependencies not installed. (If this is during installation of the add-on, this is as expected. If it is while building, serving, or testing the application, this is an error.)");
+      return;
+    }
+
     try {
       var plugin = new TsPreprocessor({includeExtensions: ['.ts','.js']});
       registry.add('js', plugin);
