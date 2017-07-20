d47ca49cea616cda3978fa21b81b42f272d85eb2
Chris Krycho <chris@chriskrycho.com>
2017-07-20 16:08:34 -0400

    Use this.ui.write instead of console.log.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 /* eslint-env node */
-
-'use strict';
-var path      = require('path');
-var process = require('process');
+const path = require('path');
 
 let TsPreprocessor;
 try {
   TsPreprocessor = require('./lib/typescript-preprocessor');
-} catch ( ex ) {
+} catch (ex) {
   // Do nothing; we just won't have the plugin available. This means that if you
   // somehow end up in a state where it doesn't load, the preprocessor *will*
   // fail, but this is necessary because the preprocessor depends on packages
@@ -17,30 +14,33 @@ try {
 module.exports = {
   name: 'ember-cli-typescript',
 
-
-  included: function(app) {
+  included(app) {
     this._super.included.apply(this, arguments);
     this.app = app;
-
   },
 
-  blueprintsPath: function() {
+  blueprintsPath() {
     return path.join(__dirname, 'blueprints');
   },
 
   setupPreprocessorRegistry: function(type, registry) {
     if (!TsPreprocessor) {
-      console.log("Note: TypeScript preprocessor not available -- some dependencies not installed. (If this is during installation of the add-on, this is as expected. If it is while building, serving, or testing the application, this is an error.)");
+      this.ui.write(
+        'Note: TypeScript preprocessor not available -- some dependencies not installed. ' +
+          '(If this is during installation of the add-on, this is as expected. If it is ' +
+          'while building, serving, or testing the application, this is an error.)'
+      );
       return;
     }
 
     try {
-      var plugin = new TsPreprocessor({includeExtensions: ['.ts','.js']});
+      const plugin = new TsPreprocessor({ includeExtensions: ['.ts', '.js'] });
       registry.add('js', plugin);
-    } catch ( ex ) {
-      console.log( "Missing or invalid tsconfig.json, please fix or run `ember generate ember-cli-typescript`." );
-      console.log( '  ' + ex.toString());
+    } catch (ex) {
+      this.ui.write(
+        'Missing or invalid tsconfig.json, please fix or run `ember generate ember-cli-typescript`.'
+      );
+      this.ui.write('  ' + ex.toString());
     }
-  }
-
+  },
 };
