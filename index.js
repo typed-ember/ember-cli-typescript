456722bb20be30a4f95bed7c519f4f8356a07c7f
Chris Krycho <chris@chriskrycho.com>
2017-07-29 16:31:40 -0400

    Aaaaand, one last fix for using b-c-ts 2.0 correctly.

--- index.js
+++ index.js
@@ -14,16 +14,7 @@ try {
 module.exports = {
   name: 'ember-cli-typescript',
 
-  included(app) {
-    this._super.included.apply(this, arguments);
-    this.app = app;
-  },
-
-  blueprintsPath() {
-    return path.join(__dirname, 'blueprints');
-  },
-
-  setupPreprocessorRegistry: function(type, registry) {
+  setupPreprocessorRegistry(type, registry) {
     if (!TsPreprocessor) {
       this.ui.write(
         'Note: TypeScript preprocessor not available -- some dependencies not installed. ' +
@@ -34,8 +25,7 @@ module.exports = {
     }
 
     try {
-      const plugin = new TsPreprocessor({ includeExtensions: ['.ts', '.js'] });
-      registry.add('js', plugin);
+      registry.add('js', new TsPreprocessor());
     } catch (ex) {
       this.ui.write(
         'Missing or invalid tsconfig.json, please fix or run `ember generate ember-cli-typescript`.'
