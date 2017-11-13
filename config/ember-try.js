247cf6eef2336594b85caec379020e1f415a1201
Chris Krycho <chris@chriskrycho.com>
2017-11-13 08:21:01 -0700

    Set Prettier rules.

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,87 +5,87 @@ module.exports = {
       name: 'ember-lts-2.4',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-4'
+          ember: 'components/ember#lts-2-4',
         },
         resolutions: {
-          'ember': 'lts-2-4'
-        }
+          ember: 'lts-2-4',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-8'
+          ember: 'components/ember#lts-2-8',
         },
         resolutions: {
-          'ember': 'lts-2-8'
-        }
+          ember: 'lts-2-8',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-release',
       bower: {
         dependencies: {
-          'ember': 'components/ember#release'
+          ember: 'components/ember#release',
         },
         resolutions: {
-          'ember': 'release'
-        }
+          ember: 'release',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-beta',
       bower: {
         dependencies: {
-          'ember': 'components/ember#beta'
+          ember: 'components/ember#beta',
         },
         resolutions: {
-          'ember': 'beta'
-        }
+          ember: 'beta',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-canary',
       bower: {
         dependencies: {
-          'ember': 'components/ember#canary'
+          ember: 'components/ember#canary',
         },
         resolutions: {
-          'ember': 'canary'
-        }
+          ember: 'canary',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-default',
       npm: {
-        devDependencies: {}
-      }
-    }
-  ]
+        devDependencies: {},
+      },
+    },
+  ],
 };
--- tests/dummy/app/components/test-one.js
+++ tests/dummy/app/components/test-one.js
@@ -1,6 +1,5 @@
-
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  someValue: 'from component'
+  someValue: 'from component',
 });
