5d6d8e28e954736938c655310a17c7a0e314a135
Chris Krycho <chris@chriskrycho.com>
2017-12-17 11:22:49 -0700

    Upgrade Ember from 2.13.3 to 2.17.1.

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,36 +2,20 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#lts-2-4',
-        },
-        resolutions: {
-          ember: 'lts-2-4',
-        },
-      },
+      name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
-          'ember-source': null,
-        },
-      },
+          'ember-source': '~2.12.0'
+        }
+      }
     },
     {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#lts-2-8',
-        },
-        resolutions: {
-          ember: 'lts-2-8',
-        },
-      },
+      name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': null,
-        },
-      },
+          'ember-source': '~2.16.0'
+        }
+      }
     },
     {
       name: 'ember-release',
