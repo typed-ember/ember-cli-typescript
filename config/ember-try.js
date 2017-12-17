ef7f7609df65d90fd3e5dd0f8cb600a9f59c853b
Chris Krycho <chris@chriskrycho.com>
2017-12-17 15:22:47 -0700

    Use yarn in ember-try.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,21 +1,22 @@
 /* eslint-env node */
 module.exports = {
+  useYarn: true,
   scenarios: [
     {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
-          'ember-source': '~2.12.0'
-        }
-      }
+          'ember-source': '~2.12.0',
+        },
+      },
     },
     {
       name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': '~2.16.0'
-        }
-      }
+          'ember-source': '~2.16.0',
+        },
+      },
     },
     {
       name: 'ember-release',
