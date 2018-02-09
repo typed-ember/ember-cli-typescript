63e24534c958447c565a56b1af6d7def0bf1942b
Marco Umpierrez <marcoumpierrez@gmail.com>
2018-02-09 10:01:23 +0100

    Add `controller` generator

--- node-tests/blueprints/controller-test.js
+++ node-tests/blueprints/controller-test.js
@@ -10,17 +10,16 @@ const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 describe('Acceptance: ember generate and destroy controller', function() {
   setupTestHooks(this);
 
-  it('controller foo-bar', function() {
+  it('controller foo', function() {
     let args = ['controller', 'foo-bar'];
 
     // pass any additional command line options in the arguments array
-    return emberNew().then(() =>
-      emberGenerateDestroy(args, file => {
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, (file) => {
         const generated = file('app/controllers/foo-bar.ts');
         expect(generated).to.contain('class FooBar extends Controller');
         expect(generated).to.contain('interface Registry');
         expect(generated).to.contain("'foo-bar': FooBar");
-      })
-    );
+    }));
   });
 });
