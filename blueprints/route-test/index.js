e437db97730d46fc3db34d2d5d40933b1b848812
Chris Krycho <chris@chriskrycho.com>
2018-02-10 23:34:55 -0700

    Add route, route-test generator.
    Note that we're intentionally skipping a couple edge-case-ish scenarios
    here, because they're sufficiently rare, and this resolution issue seems
    to be kind of buggy on the Ember CLI side, that it's not worth pursuing
    further at present.
    
    Also, we do not (and cannot) generate or update the `router.ts` file at
    present; we leave it as JavaScript. We'll need upstream changes to
    ember-cli to support that.

---
+++ blueprints/route-test/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const stringUtil = require('ember-cli-string-utils');
+const useTestFrameworkDetector = require('../test-framework-detector');
+
+module.exports = useTestFrameworkDetector({
+  description: 'Generates a route unit test.',
+
+  availableOptions: [
+    {
+      name: 'reset-namespace',
+      type: Boolean
+    }
+  ],
+
+  fileMapTokens: function() {
+    return {
+      __test__: function (options) {
+        let moduleName = options.locals.moduleName;
+
+        if (options.pod) {
+          moduleName = 'route';
+        }
+
+        return `${moduleName}-test`;
+      },
+      __path__: function(options) {
+        if (options.pod) {
+          return path.join(options.podPath, options.locals.moduleName);
+        }
+        return 'routes';
+      }
+    };
+  },
+
+  locals: function(options) {
+    let moduleName = options.entity.name;
+
+    if (options.resetNamespace) {
+      moduleName = moduleName.split('/').pop();
+    }
+
+    return {
+      friendlyTestDescription: ['Unit', 'Route', options.entity.name].join(' | '),
+      moduleName: stringUtil.dasherize(moduleName)
+    };
+  },
+});
