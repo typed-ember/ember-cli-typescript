a5c8908421b5ac93469b894fc0ea414aa2f413ba
Dan Freeman <dfreeman@salsify.com>
2018-07-25 17:04:40 -0400

    Fix MU generator acceptance tests

--- tests/unit/build-test.ts
+++ tests/unit/build-test.ts
@@ -22,12 +22,12 @@ module('Unit | Build', function() {
   test('app files aren\'t shadowed by addons\' app tree files', function(assert) {
     assert.equal(shadowedFile, 'dummy/shadowed-file');
   });
-});
 
-test('MU app files wind up in the right place', function(assert) {
-  assert.equal(muFile, 'dummy/src/test-file');
-});
+  test('MU app files wind up in the right place', function(assert) {
+    assert.equal(muFile, 'dummy/src/test-file');
+  });
 
-test('MU addon files wind up in the right place', function(assert) {
-  assert.equal(addonFileC, 'in-repo-c/src/test-file');
+  test('MU addon files wind up in the right place', function(assert) {
+    assert.equal(addonFileC, 'in-repo-c/src/test-file');
+  });
 });
