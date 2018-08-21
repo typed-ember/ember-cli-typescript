6762e7bf1b69c22ec5e5f61d75bc63b433bb80d6
Dan Freeman <dfreeman@salsify.com>
2018-08-20 21:19:34 -0400

    Deletions

--- blueprints/component/files/__root__/__path__/__name__.ts
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-<%= importTemplate %>
-export default class <%= classifiedModuleName %> extends Component.extend({
-  // anything which *must* be merged to prototype here
-}) {<%= contents %>
-  // normal class body definition here
-};
