37983717076c75d76849b0acd102bbb672f1a566
Dan Freeman <dfreeman@salsify.com>
2018-10-03 14:30:14 -0400

    chore: Remove specialized MU build tests

--- tests/dummy/lib/in-repo-c/src/test-file.ts
+++
@@ -1,4 +0,0 @@
-// This should wind up in the addon tree
-const value: string = 'in-repo-c/src/test-file';
-
-export default value;
--- tests/unit/build-test.ts
+++ tests/unit/build-test.ts
@@ -2,10 +2,8 @@ import { module, test } from 'qunit';
 
 import addonFileA from 'in-repo-a/test-file';
 import addonFileB from 'in-repo-b/test-file';
-import addonFileC from 'in-repo-c/src/test-file';
 import fileA from 'dummy/a';
 import fileB from 'dummy/b';
-import muFile from 'dummy/src/test-file';
 import shadowedFile from 'dummy/shadowed-file';
 import { description as fromAts } from 'in-repo-a/test-support/from-ats';
 import { description as fromTs } from 'dummy/tests/from-ts';
@@ -25,18 +23,11 @@ module('Unit | Build', function() {
     assert.equal(shadowedFile, 'dummy/shadowed-file');
   });
 
-  test('MU app files wind up in the right place', function(assert) {
-    assert.equal(muFile, 'dummy/src/test-file');
-  });
-
-  test('MU addon files wind up in the right place', function(assert) {
-    assert.equal(addonFileC, 'in-repo-c/src/test-file');
-  });
-
   test('addon\'s addon-test-support files end up in <addon-name>/test-support/*', function (assert) {
     assert.ok(fromAts);
     assert.equal(fromAts, 'From addon-test-support');
   });
+
   test('addon\'s test-support files end up in dummy/tests/*', function (assert) {
     assert.ok(fromTs);
     assert.equal(fromTs, 'From test-support');
