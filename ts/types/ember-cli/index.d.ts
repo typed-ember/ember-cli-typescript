b73eadd40ab925da518be1cfe0426386f4ebff75
Dan Freeman <dfreeman@salsify.com>
2018-10-03 14:26:26 -0400

    chore: Introduce local typings for ember-cli

---
+++ ts/types/ember-cli/index.d.ts
@@ -0,0 +1,61 @@
+declare module 'ember-cli/lib/broccoli/ember-app' {
+  import CoreObject from 'core-object';
+
+  export default class EmberApp extends CoreObject {
+    options?: Record<string, unknown>;
+  }
+}
+
+declare module 'ember-cli/lib/models/addon' {
+  import CoreObject, { ExtendOptions } from 'core-object';
+  import UI from 'console-ui';
+  import Project from 'ember-cli/lib/models/project';
+  import Command from 'ember-cli/lib/models/command';
+  import EmberApp from 'ember-cli/lib/broccoli/ember-app';
+
+  export default class Addon extends CoreObject {
+    name: string;
+    root: string;
+    app?: EmberApp;
+    parent: Addon | Project;
+    project: Project;
+    addons: Addon[];
+    ui: UI;
+    options?: Record<string, unknown>;
+    pkg: {
+      name: string;
+      version: string;
+      dependencies?: Record<string, string>;
+      devDependencies?: Record<string, string>;
+    };
+
+    blueprintsPath(): string;
+    included(includer: EmberApp | Project): void;
+    includedCommands(): Record<string, typeof Command | ExtendOptions<Command>> | void;
+    shouldIncludeChildAddon(addon: Addon): boolean;
+    isDevelopingAddon(): boolean;
+  }
+}
+
+declare module 'ember-cli/lib/models/command' {
+  import CoreObject from 'core-object';
+  import Project from 'ember-cli/lib/models/project';
+
+  export default class Command extends CoreObject {}
+}
+
+declare module 'ember-cli/lib/models/project' {
+  import CoreObject from 'core-object';
+  import UI from 'console-ui';
+  import Addon from 'ember-cli/lib/models/addon';
+
+  export default class Project extends CoreObject {
+    ui: UI;
+    root: string;
+    addons: Addon[];
+
+    name(): string;
+    isEmberCLIAddon(): boolean;
+    require(module: string): unknown;
+  }
+}
