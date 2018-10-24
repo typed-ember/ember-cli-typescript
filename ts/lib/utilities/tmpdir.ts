8218b7d95fcca68057cd9a71c100d31209d980e4
Dan Freeman <dfreeman@salsify.com>
2018-10-24 18:29:39 -0400

    chore: Remove dead code

--- ts/lib/utilities/tmpdir.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import * as os from 'os';
-import * as fs from 'fs';
-
-// Watchman gets upset when it receives non-canonical paths as watch roots
-export = () => fs.realpathSync(os.tmpdir());
