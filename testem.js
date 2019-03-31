c8776dd1496dbce26c06ff81e48b1761afc9abb3
Mike North <michael.l.north@gmail.com>
2019-03-31 12:45:05 -0700

    ci(azure): linux, compatibility, firefox testing in Azure Pipelines

--- testem.js
+++ testem.js
@@ -5,9 +5,11 @@ module.exports = {
   report_file: 'ember-tests.xml',
   xunit_exclude_stack: true, // we *probably* want this on to keep the xunit file clean
   xunit_intermediate_output: true,
-  launch_in_ci: ['Chrome'],
+  launch_in_ci: ['Chrome', 'Firefox'],
   launch_in_dev: ['Chrome'],
+  browser_start_timeout: 60000,
   browser_args: {
+    Firefox: { ci: ['-headless', '--window-size=1440,900'] },
     Chrome: {
       mode: 'ci',
       // prettier-disable -- args are useful to have in a sane order
@@ -21,7 +23,7 @@ module.exports = {
           --no-sandbox is needed when running Chrome inside a container.
           See https://github.com/ember-cli/ember-cli-chai/pull/45/files.
         */
-        process.env.TRAVIS ? '--no-sandbox' : []
+        (process.env.TRAVIS || process.env.CI) ? '--no-sandbox' : []
       ),
     },
   },
