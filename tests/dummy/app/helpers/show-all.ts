5766e28c289672372ddbb64eb82dd0d73bac2570
Chris Krycho <hello@chriskrycho.com>
2020-01-20 09:52:59 -0700

    docs: clarify example in helper docs
    Simplify the `showAll` helper so that it is easier to follow. Extract
    a small helper function as part of that clarification.

--- tests/dummy/app/helpers/show-all.ts
+++ tests/dummy/app/helpers/show-all.ts
@@ -5,27 +5,25 @@ interface Dict<T = unknown> {
 // BEGIN-SNIPPET show-all.ts
 import { helper } from '@ember/component/helper';
 
+const describe = (entries: string): string => (entries.length > 0 ? entries : '(none)');
+
 export function showAll(positional: unknown[], named: Dict) {
   // pretty print each item with its index, like `0: { neat: true }` or
   // `1: undefined`.
-  const positionalDescription =
-    'positional: ' +
-    positional
-      .reduce<string[]>((items, arg, index) => items.concat(`${index}: ${JSON.stringify(arg)}`), [])
-      .join(', ');
+  const positionalEntries = positional
+    .reduce<string[]>((items, arg, index) => items.concat(`${index}: ${JSON.stringify(arg)}`), [])
+    .join(', ');
 
   // pretty print each item with its name, like `cool: beans` or
   // `answer: 42`.
-  const namedDescription =
-    'named: ' +
-    Object.keys(named)
-      .reduce<string[]>(
-        (items, key) => items.concat(`${key}: ${JSON.stringify(named[key], undefined, 2)}`),
-        []
-      )
-      .join(', ');
+  const namedEntries = Object.keys(named)
+    .reduce<string[]>(
+      (items, key) => items.concat(`${key}: ${JSON.stringify(named[key], undefined, 2)}`),
+      []
+    )
+    .join(', ');
 
-  return `${positionalDescription}\n${namedDescription}`;
+  return `positional: ${describe(positionalEntries)}\nnamed: ${describe(namedEntries)}`;
 }
 
 export default helper(showAll);
