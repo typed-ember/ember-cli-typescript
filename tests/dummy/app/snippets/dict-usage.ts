049b0950a40a3eb45313d22e9ea7dbe17ef56dda
Chris Krycho <hello@chriskrycho.com>
2020-11-23 17:32:05 -0700

    docs: drop now-unused snippets

--- tests/dummy/app/snippets/dict-usage.ts
+++
@@ -1,8 +0,0 @@
-// BEGIN-SNIPPET dict-usage.ts
-let ages: Dict<number> = {
-  chris: 32,
-};
-
-let johnAge = ages['john']; // undefined
-let chrisAge = ages['chris']; // 32
-// END-SNIPPET
--- tests/dummy/app/snippets/dict.ts
+++
@@ -1,5 +0,0 @@
-// BEGIN-SNIPPET dict.ts
-interface Dict<T = unknown> {
-  [key: string]: T | undefined;
-}
-// END-SNIPPET
