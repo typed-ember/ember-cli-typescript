5543bba7a7daa43187ce5d0500d9cdbe78785de2
Chris Krycho <hello@chriskrycho.com>
2020-11-23 17:40:18 -0700

    docs: remove addon-docs from dummy app router

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,67 +1,11 @@
-import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = AddonDocsRouter.extend({
+const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
 });
 
-Router.map(function() {
-  docsRoute(this, function() {
-    this.route('getting-started', function() {
-      this.route('installation');
-      this.route('configuration');
-    });
-
-    this.route('ts', function() {
-      this.route('overview');
-      this.route('decorators');
-      this.route('with-addons');
-      this.route('using-ts-effectively');
-      this.route('current-limitations');
-    });
-
-    this.route('ember', function() {
-      this.route('overview');
-      this.route('components');
-      this.route('services');
-      this.route('testing');
-      this.route('routes');
-      this.route('controllers');
-      this.route('helpers');
-      this.route('apps-and-addons');
-    });
-
-    this.route('ember-data', function() {
-      this.route('overview');
-      this.route('models');
-      // this.route('adapters');
-      // this.route('serializers');
-      // this.route('transforms');
-    });
-
-    this.route('legacy', function() {
-      this.route('overview');
-      this.route('ember-object');
-      this.route('computed-properties');
-      this.route('mixins');
-      // this.route('ember-component');
-    });
-
-    this.route('cookbook', function() {
-      this.route('overview');
-      this.route('working-with-route-models');
-    });
-
-    this.route('upgrade-notes');
-
-    this.route('troubleshooting', function() {
-      this.route('conflicting-types');
-    });
-    this.route('type-defs', function () {
-      this.route('package-names');
-    });
-  });
-});
+Router.map(function() {});
 
 export default Router;
