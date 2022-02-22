1c4ef95d89cd9a7cacfa109d8359c8fe277c97f5
Chris Krycho <hello@chriskrycho.com>
2022-02-22 14:48:46 -0700

    add an explanation to eslint-disable

--- ts/blueprints/ember-cli-typescript/index.js
+++ ts/blueprints/ember-cli-typescript/index.js
@@ -6,7 +6,10 @@ const path = require('path');
 const APP_DECLARATIONS = `import Ember from 'ember';
 
 declare global {
-  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {} // eslint-disable-line @typescript-eslint/no-empty-interface
+  // Prevents ESLint from "fixing" this via its auto-fix to turn it into a type
+  // alias (e.g. after running any Ember CLI generator)
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
   // interface Function extends Ember.FunctionPrototypeExtensions {}
 }
 
