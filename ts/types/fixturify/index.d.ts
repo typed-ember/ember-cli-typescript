7e7df1e2929cd584fedece48d3617b838446dc48
Chris Krycho <hello@chriskrycho.com>
2022-10-25 17:01:04 -0600

    Update to current fixturify and drop types for it

--- ts/types/fixturify/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'fixturify' {
-  export interface Directory {
-    [key: string]: string | Directory;
-  }
-
-  export function readSync(path: string): Directory;
-  export function writeSync(path: string, contents: Directory): void;
-}
