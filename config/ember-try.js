82dc90ddce7fa3933ca7272e5e90cc77eaf3745c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-10 14:43:32 -0400

    Copy part of ember-try config from another repo

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,48 +1,62 @@
 const getChannelUrl = require('ember-source-channel-url');
 
-module.exports = {
-  useYarn: true,
-  command: 'yarn ci:test',
-  scenarios: [
-    {
-      name: 'defaults',
-      npm: {
-        devDependencies: {},
-      },
-    },
-    {
-      name: 'typescript-release',
-      npm: {
-        devDependencies: {
-          typescript: 'latest',
+module.exports = async function () {
+  const ember5Deps = {
+    '@ember/string': '^3.1.1',
+    '@ember/test-helpers': '^3.2.0',
+    'ember-qunit': '^7.0.0',
+    'ember-resolver': '^11.0.0',
+    'ember-auto-import': '^2.3.0',
+    'ember-cli': '^5.1.0',
+    'ember-maybe-import-regenerator': null,
+  };
+
+  return {
+    useYarn: true,
+    command: 'yarn ci:test',
+    scenarios: [
+      {
+        name: 'defaults',
+        npm: {
+          devDependencies: {},
         },
       },
-    },
-    {
-      name: 'typescript-beta',
-      npm: {
-        devDependencies: {
-          typescript: 'next',
+      {
+        name: 'typescript-release',
+        npm: {
+          devDependencies: {
+            typescript: 'latest',
+          },
         },
       },
-    },
-    {
-      name: 'ember-cli-release',
-      npm: {
-        devDependencies: {
-          'ember-cli': 'latest',
-          'ember-source': await getChannelUrl('release'),
+      {
+        name: 'typescript-beta',
+        npm: {
+          devDependencies: {
+            typescript: 'next',
+          },
         },
       },
-    },
-    {
-      name: 'ember-cli-beta',
-      npm: {
-        devDependencies: {
-          'ember-cli': 'beta',
-          'ember-source': await getChannelUrl('beta'),
+      {
+        name: 'ember-cli-release',
+        npm: {
+          devDependencies: {
+            'ember-cli': 'latest',
+            'ember-source': await getChannelUrl('release'),
+            ...ember5Deps,
+          },
         },
       },
-    },
-  ],
+      {
+        name: 'ember-cli-beta',
+        npm: {
+          devDependencies: {
+            'ember-cli': 'beta',
+            'ember-source': await getChannelUrl('beta'),
+            ...ember5Deps,
+          },
+        },
+      },
+    ],
+  };
 };
